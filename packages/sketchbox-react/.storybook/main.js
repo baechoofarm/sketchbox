759b10c04f9a62dacc5c342648a60045b37bda40
Byeonggeol Ha <byeonggeol98@gmail.com>
2021-08-25 17:56:14 +0900

    Set storybook on sketchbox-react package

---
+++ packages/sketchbox-react/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  typescript: {
+    check: false
+  },
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
