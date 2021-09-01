f2c7b11fc545d118d5db9e12b8b0ef3abbe12aeb
Byeonggeol Ha <byeonggeol98@gmail.com>
2021-09-01 15:47:59 +0900

    Define Link Element (#4)

--- packages/sketchbox-react/src/editor/elements/link/LinkElementItem.tsx
+++ packages/sketchbox-react/src/editor/elements/link/LinkElementItem.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import {LinkElement} from "sketchbox";
 import {SketchboxElementProps} from "../../../internal";
 
-export interface LinkElementItemProps extends SketchboxElementProps {
+export interface LinkElementItemProps extends SketchboxElementProps<LinkElement> {
 
 }
 
-const LinkElementItem: React.FC<LinkElementItemProps> = () => {
-    return null;
+const LinkElementItem: React.FC<LinkElementItemProps> = ({
+    attributes,
+    element,
+    children
+}) => {
+    return (
+        <a {...attributes} href={element.url}>
+            {children}
+        </a>
+    );
 };
 
 export {LinkElementItem};
